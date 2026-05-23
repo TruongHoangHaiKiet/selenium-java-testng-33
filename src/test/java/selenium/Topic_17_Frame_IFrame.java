ebf758d478c9816324c405f7e18e9d817d6f58b6
kietthh <kietthh@vng.com.vn>
2026-05-23 22:46:29 +0700

    Done Topic 40 (Handle Frame/ Iframe Part II)

--- src/test/java/selenium/Topic_17_Frame_IFrame.java
+++ src/test/java/selenium/Topic_17_Frame_IFrame.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -25,7 +26,7 @@ public class Topic_17_Frame_IFrame {
     }
 
     @Test
-    public void TC_01_() throws InterruptedException {
+    public void TC_01_iframe() throws InterruptedException {
         // Home (site A)
         driver.get("https://toidicodedao.com/");
 
@@ -52,8 +53,49 @@ public class Topic_17_Frame_IFrame {
     }
 
     @Test
-    public void TC_02_() {
+    public void TC_02_Jquery() throws InterruptedException {
+        driver.get("https://jqueryui.com/dialog/");
+        // Switch to iframe
+        driver.switchTo().frame(driver.findElement(By.cssSelector("div#content iframe.demo-frame")));
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.ui-dialog")).isDisplayed());
+        driver.findElement(By.cssSelector("button.ui-dialog-titlebar-close")).click();
+        Thread.sleep(3000);
 
+        Assert.assertFalse(driver.findElement(By.cssSelector("div.ui-dialog")).isDisplayed());
+        // Quay ve Main
+        driver.switchTo().defaultContent();
+        driver.findElement(By.cssSelector("input[name='s']")).sendKeys("Dialog");
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void TC_03_FormSite() throws InterruptedException{
+        driver.get("https://www.formsite.com/templates/education/campus-safety-survey/");
+        driver.findElement((By.cssSelector("img[alt='Campus Safety Survey']"))).click();
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe#frame-one85593366")));
+        new Select(driver.findElement(By.xpath("//label[contains(text(),'Year')]/following-sibling::select"))).selectByVisibleText("Sophomore");
+        new Select(driver.findElement(By.xpath("//label[contains(text(),'Residence')]/following-sibling::select"))).selectByVisibleText("West Dorm");
+        new Select(driver.findElement(By.xpath("//label[contains(string(),'Public Safety')]/following-sibling::select"))).selectByVisibleText("Good");
+        driver.findElement(By.xpath("//input[@id='FSsubmit']")).isDisplayed();
+        driver.switchTo().defaultContent();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[contains(text(),'Class Registration Form')]")).click();
+
+    }
+
+    @Test
+    public void TC_04_FormSite() throws InterruptedException{
+        driver.get("https://netbanking.hdfcbank.com/netbanking/");
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame")));
+        driver.findElement(By.cssSelector("input[name='fldLoginUserId']")).sendKeys("john.terry");
+        driver.findElement(By.cssSelector("a.login-btn")).click();
+        driver.switchTo().defaultContent();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("input#keyboard")).sendKeys("john.terry2026");
+        driver.findElement(By.cssSelector("a.loginBtn")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.cssSelector("p.error-msg")).getText(),"abc");
     }
 
     @AfterClass
